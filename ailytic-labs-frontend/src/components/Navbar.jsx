598e8f6d6fa2966eb43f73ec2b50d65e2d9ec2fe
Odhiambo-20 <odhiambov110@gmail.com>
2025-05-29 08:12:44 -0400

    Made changes to  the Navbar component

--- ailytic-labs-frontend/src/components/Navbar.jsx
+++ ailytic-labs-frontend/src/components/Navbar.jsx
@@ -6,10 +6,11 @@ function Navbar() {
       <h1 className="text-2xl font-bold">AIlytic Labs</h1>
       <ul className="flex gap-4">
         <li><Link to="/" className="hover:text-blue-400">Home</Link></li>
-        <li><Link to="/about" className="hover:text-blue-400">About</Link></li>
-        <li><Link to="/products" className="hover:text-blue-400">Products</Link></li>
-        <li><Link to="/demo" className="hover:text-blue-400">Demo</Link></li>
-        <li><Link to="/contact" className="hover:text-blue-400">Contact</Link></li>
+        <li><Link to="/about" className="hover:text-blue-400">Robots</Link></li>
+        <li><Link to="/products" className="hover:text-blue-400">Drones</Link></li>
+        <li><Link to="/demo" className="hover:text-blue-400">Applications</Link></li>
+        <li><Link to="/contact" className="hover:text-blue-400">Sandbox Lab</Link></li>
+        <li><Link to="/contact" className="hover:text-blue-400">Company</Link></li>
       </ul>
     </nav>
   );
