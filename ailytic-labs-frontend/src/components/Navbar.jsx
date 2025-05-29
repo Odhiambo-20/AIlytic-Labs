44c658662aa46d20fcd371e44c01ebd14c768d58
Odhiambo-20 <odhiambov110@gmail.com>
2025-05-29 09:46:27 -0400

    Create Navbar component

--- ailytic-labs-frontend/src/components/Navbar.jsx
+++ ailytic-labs-frontend/src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="bg-gray-900 p-4 text-white flex justify-between items-center">
-      <h1 className="text-2xl font-bold">AIlytic Labs</h1>
+      <Link to='/'>
+      <h1 className="text-2xl font-bold txt-sm sm:text-xl flex flex-wrap">
+        <span className='text-blue-900'>AIlytics</span>
+          <span className='text-blue-900'>Labs </span>
+      </h1>
+      </Link>
       <ul className="flex gap-4">
         <li><Link to="/" className="hover:text-blue-400">Home</Link></li>
         <li><Link to="/robots" className="hover:text-blue-400">Robots</Link></li>
